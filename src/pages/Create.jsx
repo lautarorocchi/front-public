c2e220350d5ebc1185357f155b4f6dfab6ea3ab4
lautarorocchi <62039372+lautarorocchi@users.noreply.github.com>
2023-08-03 19:41:46 -0300

    az

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -35,8 +35,7 @@ function Create() {
         ProductServices.createProduct(formData)
         .then(data => {
           if (data) {
-            console.log(data)
-            /*navigate('/admin', { state: {created: "¡El producto ha sido creado! Puedes observarlo en el panel de control." } })*/
+            navigate('/admin', { state: {created: "¡El producto ha sido creado! Puedes observarlo en el panel de control." } })
           }
           else {
             navigate('/404')
