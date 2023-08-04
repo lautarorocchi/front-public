ee130a5f2f8f21dd94e1cd27377d2f3700e3e556
lautarorocchi <62039372+lautarorocchi@users.noreply.github.com>
2023-08-04 00:01:29 -0300

    Update Create.jsx

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -26,10 +26,8 @@ function Create() {
         const formData = new FormData();
         formData.append("name", data.name);
         formData.append("description", data.description);
-        formData.append("img", data.file[0].name);
         formData.append("cantidad", data.cantidad);
         formData.append("empresa_id", empresa);
-        formData.append("file", data.file[0]);
 
         ProductServices.createProduct(formData)
         .then(data => {
