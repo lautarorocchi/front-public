c5ef726d20acb2bca06e4096f5fcae0baf48c828
lautarorocchi <62039372+lautarorocchi@users.noreply.github.com>
2023-08-04 00:46:41 -0300

    Update Create.jsx

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -36,7 +36,7 @@ function Create() {
         ProductServices.createProduct(formData)
         .then(data => {
           if (data) {
-            busboy(formData)
+            console.log(busboy(data))
             /*navigate('/admin', { state: {created: "¡El producto ha sido creado! Puedes observarlo en el panel de control." } })*/
           }
           else {
