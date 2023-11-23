503745a46db7493b9a3dec6a52d13afc6902b41e
lautarorocchi <62039372+lautarorocchi@users.noreply.github.com>
2023-11-23 11:44:00 -0300

    Update CreateEmpresa.jsx

--- src/pages/CreateEmpresa.jsx
+++ src/pages/CreateEmpresa.jsx
@@ -77,7 +77,7 @@ function CreateEmpresa() {
           <div>
             <hgroup>
               <h2>Regístra una empresa</h2>
-              <h3><div>Registra una nueva empresa para anexarla a tu usuario, </div>¿Tu empresa ya esta registrada? <Link to="/registro"><u>Registrate</u></Link>.<br></br> ¿Ya tienes usuario? <Link to="/login"><u>Inicia Sesión</u></Link>.</h3>
+              <h3><div>Registra una nueva empresa para anexarla a tu usuario, </div>¿Tu empresa ya esta registrada? <Link to="/registro"><u>Registrate</u></Link>.<br></br> ¿Ya tenes usuario? <Link to="/login"><u>Inicia Sesión</u></Link>.</h3>
             </hgroup>
             <form onSubmit={handleSubmit(onSubmit)}>
               <label htmlFor='nombre' className='left'>Nombre de la empresa</label>
