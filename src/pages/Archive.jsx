8d9f0f191f28f5e7cad7c5b28e2e64cdc3b302b9
lautarorocchi <62039372+lautarorocchi@users.noreply.github.com>
2023-11-27 18:38:13 -0300

    Update Archive.jsx

--- src/pages/Archive.jsx
+++ src/pages/Archive.jsx
@@ -34,6 +34,9 @@ function Archive() {
 
   const [visibility, setVisibility] = useState(false);
 
+  const [estadoModal, setEstadoModal] = useState(false);
+
+
 
   useEffect(() => {
     setLoading(true)
@@ -101,9 +104,29 @@ function Archive() {
     setVisibility(false);
   }
 
+  function activaModal(){
+    setEstadoModal(true)
+  }
+
+  function desactivaModal(){
+    setEstadoModal(false)
+  }
 
   return (
     <div>
+    {estadoModal ? 
+      <dialog open>
+        <article>
+          <h3>¿Seguro que quieres eliminar este producto?</h3>
+          <p>
+            Al presionar eliminar, eliminarás permanente los datos de este producto, si deseas hacerlo presiona Confirmar.
+          </p>
+          <footer>
+            <span onClick={desactivaModal} role="button" className="secondary">Cancelar</span>
+            <span onClick={handleClick} role="button" className='rojo'>Confirmar</span>
+          </footer>
+        </article>
+      </dialog> : ''}
       <div>
       <section className="tables">
         <h2 className='centrado mb-1'>Administra los productos archivados de tu empresa</h2>
@@ -145,7 +168,7 @@ function Archive() {
                       <tr key={index}>
                         <td>{product.name}</td>
                         <td>{product.description}</td>
-                        <td><Link role='button' className='color-especial2'>Activar</Link></td>
+                        <td> <span onClick={activaModal} role="button" className='color-especial'>Activar producto</span></td>
                         <td>{product.cantidad}</td>
                         <td>
                           <Link to={`/producto/${product._id}`} className='mr-2'><FontAwesomeIcon icon={faCircleInfo} /></Link>
