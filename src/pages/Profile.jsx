8096ee76146102cc377cd1f65d7f22d243217717
lautarorocchi <62039372+lautarorocchi@users.noreply.github.com>
2023-12-01 20:57:38 -0300

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -79,7 +79,7 @@ function Profile() {
             </div>
             <hgroup>
               <h2>Mi perfil</h2>
-              <h3><div>Esta es la información de tu perfil,</div>¿Quieres modificar algún dato? Podes hacerlo en <Link to="/perfil/editar"><u>Edíta tu perfil</u></Link>.</h3>
+              <p>Esta es la información de tu perfil,<span className='span'>¿Quieres modificar algún dato? Podes hacerlo en <Link to="/perfil/editar"><u>Edíta tu perfil</u></Link>.</span></p>
             </hgroup>
             {(usuario) ?
               <div className='grid'>
