a378c410c18808180a25a0ad2905df149b199d4d
lautarorocchi <62039372+lautarorocchi@users.noreply.github.com>
2023-12-01 20:58:29 -0300

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -93,7 +93,7 @@ function Register() {
             }
             <hgroup>
               <h2>Regístrate</h2>
-              <h3>¿Estás registrado? <Link to="/login"><u>Inicia sesión</u></Link>.<br></br> ¿No encontrás tu empresa en la lista? <Link to="/empresa/crear"><u>Regístra Una Empresa</u></Link>.</h3>
+              <p><span className='span'>¿Estás registrado? <Link to="/login"><u>Inicia sesión</u></Link>.</span> ¿No encontrás tu empresa en la lista? <Link to="/empresa/crear"><u>Regístra Una Empresa</u></Link>.</h3>
             </hgroup>
             <form onSubmit={handleSubmit(submitUsuario)}>
               <label htmlFor='nombre' className='left'>Nombre</label>
