6d2bb678b835d3559b929cc33ce721492feef6bc
lautarorocchi <62039372+lautarorocchi@users.noreply.github.com>
2023-12-02 21:39:02 -0300

    Update Admin.jsx

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -200,7 +200,7 @@ function Admin() {
         <p>Necesitas solicitar acceso al adminsitrador de la empresa para poder adminsitrar los productos.</p>
       </hgroup>
       <img className="centerImg" src={accesoDenegado} alt="Acceso denegado"></img>
-      <Link to={`/perfil/verificar`} class="column color-especial"><button>Solicitar acceso</button></Link>
+      <Link to={`/perfil/verificar`} class="column"><button className='color-especial'>Solicitar acceso</button></Link>
       </article>
       }
     </div>
