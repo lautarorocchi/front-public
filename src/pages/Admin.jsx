df28bde78b238e3bfd1756ad360ab67e5cb67637
lautarorocchi <62039372+lautarorocchi@users.noreply.github.com>
2023-12-06 01:51:43 -0300

    Update Admin.jsx

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -30,7 +30,7 @@ function Admin() {
 
   const [currentPage, setCurrentPage] = useState(1);
 
-  const [postsPerPage] = useState(5);
+  const [postsPerPage] = useState(4);
 
   const location = useLocation();
 
