4b10ea1ec601b511272df18c997347c70d512469
lautarorocchi <62039372+lautarorocchi@users.noreply.github.com>
2023-12-14 17:10:34 -0300

    Update ResetPassword.jsx

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 function ResetPassword() {
   return (
     <div>
-      
+      <div className='container'>
+        <article className='centered'>
+
+        </article>
+      </div>
     </div>
   )
 }
