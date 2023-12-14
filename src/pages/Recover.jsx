54191ccfa997d67992936a8cfd46922e96a901a9
lautarorocchi <62039372+lautarorocchi@users.noreply.github.com>
2023-12-14 11:10:01 -0300

    Update Recover.jsx

--- src/pages/Recover.jsx
+++ src/pages/Recover.jsx
@@ -9,6 +9,11 @@ function Recover() {
             <h2>Recuperar contraseña</h2>
             <p className="message">Ingresá el mail de tu cuenta para reestablecer la contraseña.</p>
         </hgroup>
+        <form>
+              <label className='left'>Email</label>
+              <input type="text" placeholder="Email" aria-label="email"></input>
+              <button type="submit" className="contrast color-especial" role="button">Inicia sesión</button>
+        </form>
         </article>
       </div>
     </div>
