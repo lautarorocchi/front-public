6c5f5bfd203455bb40439ae965eb418a21698562
lautarorocchi <62039372+lautarorocchi@users.noreply.github.com>
2023-12-14 11:02:27 -0300

    Update Recover.jsx

--- src/pages/Recover.jsx
+++ src/pages/Recover.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 function Recover() {
   return (
     <div>
-      
+      <div className='container'>
+        <article className='centered'>
+
+        </article>
+      </div>
     </div>
   )
 }
