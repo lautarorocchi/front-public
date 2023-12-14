22cbb6698af1dd5badf8ed8cfb9769d681095da5
lautarorocchi <62039372+lautarorocchi@users.noreply.github.com>
2023-12-14 17:13:40 -0300

    Update ResetPassword.jsx

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -5,7 +5,10 @@ function ResetPassword() {
     <div>
       <div className='container'>
         <article className='centered'>
-
+            <hgroup>
+                <h2>Cambiar contraseña</h2>
+                <p className="message">Ingresa tu nueva contraseña, recuerda que no tiene que ser una clave ya utilizada en el sistema.</p>
+            </hgroup>
         </article>
       </div>
     </div>
