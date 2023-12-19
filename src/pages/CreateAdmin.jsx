b8f3a2629ece69fa3f739880d8c0f08f0a63594b
lautarorocchi <62039372+lautarorocchi@users.noreply.github.com>
2023-12-19 13:49:27 -0300

    Update CreateAdmin.jsx

--- src/pages/CreateAdmin.jsx
+++ src/pages/CreateAdmin.jsx
@@ -99,8 +99,8 @@ function createAdmin() {
                             </div> : ''
                         }
                         <hgroup>
-                            <h2>Regístrate</h2>
-                            <p><span className='span'>¿Estás registrado? <Link to="/login"><u>Inicia sesión</u></Link>.</span> ¿No encontrás tu empresa en la lista? <Link to="/empresa/crear"><u>Registrá una empresa</u></Link>.</p>
+                            <h2>Regístra tu usuario administrador</h2>
+                            <p>¿Estás registrado? <Link to="/login"><u>Inicia sesión</u></Link>.</p>
                         </hgroup>
                         <form onSubmit={handleSubmit(submitUsuario)}>
                             <label htmlFor='nombre' className='left'>Nombre</label>
