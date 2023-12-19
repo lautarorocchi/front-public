ec1dcc050f699076492e1db7b93097b8beafeaed
lautarorocchi <62039372+lautarorocchi@users.noreply.github.com>
2023-12-18 21:03:31 -0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -85,11 +85,9 @@ function Login({ onLogin }) {
                 </ul>
               </div> : ''
             }
-            <hgroup>
               <h2>Inicia sesión</h2>
               <p className="message">¿No estás registrado? <Link to="/registro"><u>Creá una cuenta</u></Link></p>
               <p className="message">¿Querés crear una empresa? <Link to="/empresa/crear"><u>Creá una empresa</u></Link></p>
-            </hgroup>
             <form onSubmit={handleSubmit(onSubmit)}>
               <label className='left' htmlFor="email">Email</label>
               <input id='email' type="text" placeholder="Email" aria-label="email" className={errors.email?.message ? 'redBorder' : ''} {...register("email")}></input>
