2a17fdbd38a2187d385d782bffcfdb9a2c3dc50d
lautarorocchi <62039372+lautarorocchi@users.noreply.github.com>
2023-12-19 16:16:54 -0300

    Update EditEmpresa.jsx

--- src/pages/EditEmpresa.jsx
+++ src/pages/EditEmpresa.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import { useParams } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import {
+    Link
+} from "react-router-dom";
 
 function EditEmpresa() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='container'>
+            <article className='centered'>
+                <div>
+                    <hgroup>
+                        <h2>Edíta tu empresa</h2>
+                        <h3><div>Edíta un empresa, </div>¿No querias editar tu empresa? Volve al <Link to="/admin"><u>Panel de control</u></Link>.</h3>
+                    </hgroup>
+                </div>
+            </article>
+        </div>
+    )
 }
 
 export default EditEmpresa
