504689de0633b5e608625303b7e61ee060d04610
lautarorocchi <62039372+lautarorocchi@users.noreply.github.com>
2023-12-19 13:48:31 -0300

    Update CreateAdmin.jsx

--- src/pages/CreateAdmin.jsx
+++ src/pages/CreateAdmin.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import {
-  Link
+    Link
 } from "react-router-dom";
 import * as userServices from '../services/user.services.js'
 import * as EmpresaServices from '../services/empresas.services.js'
@@ -14,115 +14,123 @@ import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 /* Validacion Yup Registro */
 
 const schema = yup.object({
-  name: yup.string().required("Se necesita ingresar un nombre para crear una cuenta."),
-  surname: yup.string().required("Se necesita ingresar un apellido para crear una cuenta."),
-  email: yup.string().email("El mail no es valido, revisa los datos.").required("Se necesita ingresar un Mail para ingresar al Panel de Control."),
-  password: yup.string().min(6, "El Password debe tener al menos 6 caracteres.").required("Se necesita ingresa su contraseña para ingresar al Panel de Control."),
+    name: yup.string().required("Se necesita ingresar un nombre para crear una cuenta."),
+    surname: yup.string().required("Se necesita ingresar un apellido para crear una cuenta."),
+    email: yup.string().email("El mail no es valido, revisa los datos.").required("Se necesita ingresar un Mail para ingresar al Panel de Control."),
+    password: yup.string().min(6, "El Password debe tener al menos 6 caracteres.").required("Se necesita ingresa su contraseña para ingresar al Panel de Control."),
 }).required();
 
 
 /*PONER EMPRESAS , Y SI NO ENVIAR A UN LINK PARA AGREGAR EMPRESA*/
 
 function createAdmin() {
-  const navigate = useNavigate()
-  const [passwordShown, setPasswordShown] = useState(false);
-  const [errorMensaje, setError] = useState([]);
-  const [visibility, setVisibility] = useState(false);
-  const [empresas, setEmpresas] = useState([]);
+    const navigate = useNavigate()
+    const empresa = localStorage.getItem('empresa');
+    const [passwordShown, setPasswordShown] = useState(false);
+    const [alertMensaje, setAlertMensaje] = useState([]);
+    const [visibility, setVisibility] = useState(false);
+    const [errorMensaje, setError] = useState([]);
+    const [visibility2, setVisibility2] = useState(false);
 
-  /*let defaultValues = {};*/
+    /*let defaultValues = {};*/
 
-  const { register, handleSubmit, reset, watch, formState: { errors } } = useForm({
-    resolver: yupResolver(schema)
-  });
+    const { register, handleSubmit, reset, watch, formState: { errors } } = useForm({
+        resolver: yupResolver(schema)
+    });
 
-  /*Funcion creacion del usuario*/
-
-  function submitUsuario(data) {
-    userServices.createUser(data.name, data.surname, data.empresa, data.email, data.password)
-      .then(data => {
-        if (data) {
-          navigate('/login', { state: {register: "¡Se ha registrado tu usuario con éxito! Ya podés iniciar sesión." } });
+    useEffect(() => {
+        if (location.state != null) {
+            const { admin } = location.state;
+            setAlert(admin);
+            setVisibility(true);
         }
-        else {
-          navigate('/404')
-        }
-      })
-      .catch((error) => {
-        setError(error.message);
-        setVisibility(true);
-      });
-  }
+    }, [location.state]);
 
-  useEffect(() => {
-    EmpresaServices.find()
-      .then(data => {
-        if (data) {
-          setEmpresas(data)
-          /*console.log(data)*/
-          /*reset({ ...defaultValues });*/
-        }
-        else {
-          navigate('/404')
-        }
-      })
-      .catch(err => {
-        navigate('/404')
-      })
-  }, [])
 
-  function togglePassword() {
-    setPasswordShown(!passwordShown);
-  };
+    /*Funcion creacion del usuario*/
 
-  function handleClose() {
-    setVisibility(false);
-  }
+    function submitUsuario(data) {
+        userServices.createUser(data.name, data.surname, empresa, data.email, data.password)
+            .then(data => {
+                if (data) {
+                    navigate('/login', { state: { admin: "¡Se ha registrado tu usuario administador con éxito! Ya podés iniciar sesión." } });
+                }
+                else {
+                    navigate('/404')
+                }
+            })
+            .catch((error) => {
+                setError(error.message);
+                setVisibility2(true);
+            });
+    }
 
-  return (
-    <div>
-      <div className="container">
-        <article className="centered">
-          <div>
-            {visibility ?
-              <div className="alertAmarillo">
-                <span className="closebtnAmarillo" onClick={handleClose}>&times;</span>
-                {errorMensaje}
-              </div> : ''
-            }
-            <hgroup>
-              <h2>Regístrate</h2>
-              <p><span className='span'>¿Estás registrado? <Link to="/login"><u>Inicia sesión</u></Link>.</span> ¿No encontrás tu empresa en la lista? <Link to="/empresa/crear"><u>Registrá una empresa</u></Link>.</p>
-            </hgroup>
-            <form onSubmit={handleSubmit(submitUsuario)}>
-              <label htmlFor='nombre' className='left'>Nombre</label>
-              <input id='nombre' type="text" name="nombre" placeholder="Nombre" aria-label="nombre" className={errors.name?.message ? 'redBorder' : ''} {...register("name")}></input>
-              {
-                errors.name?.message ? <p className='errorYup'>{errors.name?.message}</p> : ''
-              }
-              <label htmlFor='surname' className='left'>Apellido</label>
-              <input id='surname' type="text" name="apellido" placeholder="Apellido" aria-label="apellido" className={errors.surname?.message ? 'redBorder' : ''} {...register("surname")}></input>
-              {
-                errors.surname?.message ? <p className='errorYup'>{errors.surname?.message}</p> : ''
-              }
-              <label htmlFor='email' className='left'>Email</label>
-              <input id='email' type="text" name="email" placeholder="Email" aria-label="Login" className={errors.email?.message ? 'redBorder' : ''}  {...register("email")}></input>
-              {
-                errors.email?.message ? <p className='errorYup'>{errors.email?.message}</p> : ''
-              }
-              <label htmlFor='password' className='left'>Contraseña</label>
-              <input id='password' type={passwordShown ? "text" : "password"} name="password" placeholder="Contraseña" aria-label="Password" className={errors.password?.message ? 'redBorder' : ''} {...register("password")}></input>
-              <FontAwesomeIcon icon={passwordShown ? faEyeSlash : faEye} onClick={togglePassword} className="ojoPassword" />
-              {
-                errors.password?.message ? <p className='errorYup'>{errors.password?.message}</p> : ''
-              }
-              <button type="submit" className="contrast">Registrarte</button>
-            </form>
-          </div>
-        </article>
-      </div>
-    </div>
-  )
+    function togglePassword() {
+        setPasswordShown(!passwordShown);
+    };
+
+    function handleClose() {
+        setVisibility(false);
+    }
+
+    function handleClose2() {
+        setVisibility2(false);
+    }
+
+    return (
+        <div>
+            <div className="container">
+                <article className="centered">
+                    <div>
+                        {visibility ?
+                            <div className="alertBuena">
+                                <span className="closebtn" onClick={handleClose}>&times;</span>
+                                <ul className='white listNone'>
+                                    <li>{alertMensaje}</li>
+                                </ul>
+                            </div> : ''
+                        }
+                        {visibility2 ?
+                            <div className="alert">
+                                <span className="closebtn" onClick={handleClose2}>&times;</span>
+                                <ul className='white listNone'>
+                                    <li>{errorMensaje}</li>
+                                </ul>
+                            </div> : ''
+                        }
+                        <hgroup>
+                            <h2>Regístrate</h2>
+                            <p><span className='span'>¿Estás registrado? <Link to="/login"><u>Inicia sesión</u></Link>.</span> ¿No encontrás tu empresa en la lista? <Link to="/empresa/crear"><u>Registrá una empresa</u></Link>.</p>
+                        </hgroup>
+                        <form onSubmit={handleSubmit(submitUsuario)}>
+                            <label htmlFor='nombre' className='left'>Nombre</label>
+                            <input id='nombre' type="text" name="nombre" placeholder="Nombre" aria-label="nombre" className={errors.name?.message ? 'redBorder' : ''} {...register("name")}></input>
+                            {
+                                errors.name?.message ? <p className='errorYup'>{errors.name?.message}</p> : ''
+                            }
+                            <label htmlFor='surname' className='left'>Apellido</label>
+                            <input id='surname' type="text" name="apellido" placeholder="Apellido" aria-label="apellido" className={errors.surname?.message ? 'redBorder' : ''} {...register("surname")}></input>
+                            {
+                                errors.surname?.message ? <p className='errorYup'>{errors.surname?.message}</p> : ''
+                            }
+                            <label htmlFor='email' className='left'>Email</label>
+                            <input id='email' type="text" name="email" placeholder="Email" aria-label="Login" className={errors.email?.message ? 'redBorder' : ''}  {...register("email")}></input>
+                            {
+                                errors.email?.message ? <p className='errorYup'>{errors.email?.message}</p> : ''
+                            }
+                            <label htmlFor='password' className='left'>Contraseña</label>
+                            <input id='password' type={passwordShown ? "text" : "password"} name="password" placeholder="Contraseña" aria-label="Password" className={errors.password?.message ? 'redBorder' : ''} {...register("password")}></input>
+                            <FontAwesomeIcon icon={passwordShown ? faEyeSlash : faEye} onClick={togglePassword} className="ojoPassword" />
+                            {
+                                errors.password?.message ? <p className='errorYup'>{errors.password?.message}</p> : ''
+                            }
+                            <button type="submit" className="contrast">Registrarte</button>
+                        </form>
+                    </div>
+                </article>
+            </div>
+        </div>
+    )
 }
 
 export default createAdmin
